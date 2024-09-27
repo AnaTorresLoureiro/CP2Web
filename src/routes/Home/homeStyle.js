3e574dc70dd05969316c5c75b8f1514d1ea1d044
Lana <al.t.loureiro0@gmail.com>
2024-09-27 02:51:05 -0300

    criação de rotas

--- src/routes/Home/homeStyle.js
+++ src/routes/Home/homeStyle.js
@@ -2,5 +2,79 @@ import styled from "styled-components";
 
 export const MainHome = styled.main`
     flex-grow: 1;
+    .banner{
+        display: flex;
+        justify-content: center;
+    }
 
+    .texto{
+        position: absolute;
+        z-index: 2;
+        margin-top: 6%;
+        width: 30%;
+        color: whitesmoke;
+        font-size: 35px;
+        margin-left: 3%;
+    }
+
+    .marcas{
+        color: white;
+        margin-top: 5%;
+    }
+
+    .txt{
+        display: flex;
+        justify-content: center;
+    }
+
+    h2{
+        margin-top: 4%;
+        font-size: 30px;
+        color: white;
+        text-align: center;
+        padding: 10px;
+        width: 500px;
+        border: 5px solid white;
+    }
+
+    .info{
+        display: flex;
+        justify-content: space-around;
+        margin-top: 10%;
+        margin-bottom: 5%;
+    }
+
+    .vantagem{
+        color: white;
+        margin-bottom: 20%;
+    }
+
+    .vantagem h1{
+        width: 15%;
+    }
+
+    .assistir{
+        display: flex;
+        margin-bottom: 2%;
+    }
+
+    .video h1{
+        color: white;
+        width: 350px;
+    }
+    .line{
+        width: 5px;
+        height: 13vh;
+        background-color: #8a0015;
+        margin-left: 5%;
+    }
+
+    iframe{
+        margin-left: 25%;
+        margin-bottom: 8%;
+    }
 `
+
+
+
+
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,11 +1,53 @@
 import { MainHome } from "./homeStyle";
+import Background from '../../assets/background.png'
+import HoverCard from '../../components/Card'
+import Carousel from '../../components/Carrosel'; 
+import Terra from '../../assets/terra.png'
+import Tech from '../../assets/tech.png'
+import Mute from '../../assets/mute.png'
+import Dinheiro from '../../assets/dinheiro.png'
+
+
+
 
 export default function Home(){
 
     return(
         <>
             <MainHome>
-                
+                <h1 className="texto">O futuro da mobilidade está aqui: Conduza com eficiência e estilo.</h1>
+                <div className="banner">
+                    <img src={Background} alt="" /> 
+                </div>
+                <div className="marcas">
+                    <div className="vr"></div>
+                    <h1>Marcas</h1>
+                    <Carousel/>
+                </div>
+                <div className="txt">
+                    <h2>PORQUE TER UM CARRO ELÉTRICO</h2>
+                </div>
+                <div className="info">
+                    <HoverCard icon={Terra} hoverText="Zero emissão de carbono e menor impacto ambiental." />
+                    <HoverCard icon={Tech} hoverText="Carros conectados com sistemas de inteligência artificial e automação." />
+                    <HoverCard icon={Dinheiro} hoverText="Menor custo de manutenção e economia de combustível." />
+                    <HoverCard icon={Mute} hoverText="Condução mais suave, sem ruídos do motor." />
+                </div>
+                <div className="vantagem">
+                    <h1>VANTAGENS TOKYO DRIFT</h1>
+                    <div className="txt2">
+                        <div className="first"></div>
+                        <div className="second"></div>
+                        <div className="third"></div>
+                    </div>
+                </div>
+                <div className="video">
+                    <div className="assistir">
+                        <div className="line"> </div>
+                        <h1>Conduza o Futuro: A Revolução dos Carros Elétricos em Suas Mãos</h1>
+                    </div>
+                    <iframe width="760" height="515" src="https://www.youtube.com/embed/3B5OZhYcxrA?si=2Onv0Tbn5l56yEpk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             </MainHome>
         </>
     )
