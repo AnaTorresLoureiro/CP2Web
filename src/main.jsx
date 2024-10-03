76bce7a7581d52b74afef46a6469a1a0e5a596f4
Lana <al.t.loureiro0@gmail.com>
2024-10-02 23:36:29 -0300

    style cadprodutos

--- src/main.jsx
+++ src/main.jsx
@@ -46,6 +46,8 @@ const router = createBrowserRouter([
         path:'/listarprodutos',
         element:<Listarproduto/>,
       },
+      {path:'/editarprodutos/:id', 
+        element:<Produto/>}
     ]
   }
 ])
